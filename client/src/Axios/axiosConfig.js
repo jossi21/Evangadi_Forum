477c8c82bf482598c61f636e4dec6023d36a1b32
jossi21 <jossi9342@gmail.com>
2026-04-18 01:49:33 +0300

    updating axiosConfig

--- client/src/Axios/axiosConfig.js
+++ client/src/Axios/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseAxios = axios.create({
-  baseURL: "http://localhost:2127/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Add token to every request automatically
